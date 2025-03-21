c3891d3512495237597df085961c7709f0a4f8a3
SoSweetHam <110523856+sosweetham@users.noreply.github.com>
2025-03-21 07:38:57 +0530

    feat: add storybook (#45)
    * feat: add storybook
    
    * update: lockfile

---
+++ infrastructure/eid-wallet/.storybook/main.ts
@@ -0,0 +1,26 @@
+import type { StorybookConfig } from "@storybook/sveltekit";
+
+import { join, dirname } from "path";
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
+const config: StorybookConfig = {
+  // add back support for .svelte files when addon csf works
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|ts)"],
+  addons: [
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-svelte-csf"),
+    getAbsolutePath("@chromatic-com/storybook"),
+    getAbsolutePath("@storybook/experimental-addon-test"),
+  ],
+  framework: {
+    name: getAbsolutePath("@storybook/sveltekit"),
+    options: {},
+  },
+};
+export default config;
