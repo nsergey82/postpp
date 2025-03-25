03e143b2a73a6f94d330c812095826c1e56a39ec
Gourav Saini <gourav@auvo.io>
2025-03-25 16:44:40 +0530

    Feat/splash screen (#63)
    * feat: SplashScreen
    
    * fix: remove redundant code
    
    * fix: as per given suggestion
    
    * fix: font-size
    
    * fix: logo

--- infrastructure/eid-wallet/src/routes/+layout.svelte
+++ infrastructure/eid-wallet/src/routes/+layout.svelte
@@ -1,6 +1,26 @@
 <script lang="ts">
     import "../app.css"
     const { children } = $props();
+
+    let showSplashScreen = $state(true);
+
+    $effect(() => {
+		setTimeout(() => {
+			showSplashScreen = false;
+		}, 500);
+	});
 </script>
 
-{@render children?.()}
+{#if showSplashScreen}
+    <div class="relative w-full h-[100vh] bg-primary">
+        <img class="absolute w-full bottom-[-80px] start-0" src="/images/Shape1.svg" alt="illustration">
+        <img class="absolute w-full top-[50px] end-[100px]" src="/images/Shape2.svg" alt="illustration">
+        <div class="absolute w-full top-[42%] start-[50%] translate-x-[-50%] translate-y-[-42%]">
+            <h1 class="text-center text-white text-[44px]">eAgency</h1>
+            <p class="text-center text-white text-base">Your self-sovereign digital entity</p>
+        </div>
+        <img class="absolute bottom-[116px] start-[50%] translate-x-[-50%]" src="/images/W3DSLogoWhite.svg" alt="logo">
+    </div>
+{:else}
+    {@render children?.()}
+{/if}
