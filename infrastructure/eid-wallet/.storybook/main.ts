31d5ff84610674ffe95bac5468307fc8c7827685
Julien Connault <julien@auvo.io>
2025-03-25 14:29:28 +0300

    fix: return getAbsolutePath function to storybook (#58)
    Co-authored-by: Bekiboo <julien.connault@gmail.com>

--- infrastructure/eid-wallet/.storybook/main.ts
+++ infrastructure/eid-wallet/.storybook/main.ts
@@ -1,18 +1,22 @@
 import type { StorybookConfig } from '@storybook/sveltekit'
+import { join, dirname } from 'path'
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, 'package.json')))
+}
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|ts)'],
   addons: [
-
-    getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@chromatic-com/storybook"),
-    getAbsolutePath("@storybook/experimental-addon-test"),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/experimental-addon-test'),
   ],
   framework: {
     name: '@storybook/sveltekit',
     options: {},
   },
 
-  staticDirs: ["../static"],
-};
-export default config;
+  staticDirs: ['../static'],
+}
+export default config
