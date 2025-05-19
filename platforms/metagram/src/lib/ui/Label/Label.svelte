5c09e14ccba1ede74ff3584ee248d5528f659a8b
Gourav Saini <gourav@auvo.io>
2025-05-19 14:11:44 +0530

    feat: Label (#146)

---
+++ platforms/metagram/src/lib/ui/Label/Label.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	import type { Snippet } from 'svelte';
+	import type { HTMLLabelAttributes } from 'svelte/elements';
+
+	interface ILabelProps extends HTMLLabelAttributes {
+		children: Snippet;
+	}
+
+	let { children, ...restProps }: ILabelProps = $props();
+
+	const cBase = 'inline-block text-lg font-geist font-medium text-black-800';
+</script>
+
+<label {...restProps} class={cn([cBase, restProps.class].join(' '))}>
+	{@render children?.()}
+</label>
--- platforms/metagram/src/lib/ui/index.ts
+++ platforms/metagram/src/lib/ui/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from './Button/Button.svelte';
 export { default as Avatar } from './Avatar/Avatar.svelte';
 export { default as Input } from './Input/Input.svelte';
+export { default as Label } from './Label/Label.svelte';
 export { default as Toggle } from './Toggle/Toggle.svelte';
