0a559205c420340b9e074243afcd047cc0d76858
SoSweetHam <sosweetham@gmail.com>
2025-08-14 13:06:22 +0530

    Fix/blabsy w3ds logo (#297)
    * fix: add w3ds logo at the bottom
    
    * chore: run format

--- platforms/blabsy/src/components/login/login-main.tsx
+++ platforms/blabsy/src/components/login/login-main.tsx
@@ -110,6 +110,12 @@ export function LoginMain(): JSX.Element {
                         separation, where all your personal content is stored in
                         your own sovereign eVault, not on centralised servers.
                     </div>
+                    <Image
+                        src='/assets/w3dslogo.svg'
+                        alt='W3DS logo'
+                        width={100}
+                        height={20}
+                    />
                 </div>
             </div>
         </main>
